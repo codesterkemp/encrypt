3fb297d3eec985a050c119cc8f4293e1bdccfeed
codesterkemp <sparky_csk@yahoo.com>
2014-07-17 00:03:36 -0400

    will encode msg but will render unreadable text till smaller ascii subset.

--- encrypt/app/controllers/encrypt_controller.rb
+++ encrypt/app/controllers/encrypt_controller.rb
@@ -6,12 +6,33 @@ class EncryptController < ApplicationController
   end
 
   def encode
-    if params[:message] == "crazy"
-      redirect_to "/home"
-    else
-      @yahoo = params[:p]
+    if params[:message]
+      msg = params[:message]
+      key = params[:p]
+    
+      @decoded= xordecrypt(msg, key)
+      @msg = msg
     end
   end
+
+  def xordecrypt(msg, key, startpoint = 0)
+  rotor = startpoint
+  b = 0
+  decryptedmsg = ""
+  decryptsum = 0
+  while b < msg.length
+    plaintext = msg[b].ord^key[rotor].ord
+    decryptedmsg = decryptedmsg + plaintext.to_s + "-"
+    decryptsum = decryptsum + plaintext
+    if rotor != key.length - 1
+      rotor = rotor + 1
+    else
+      rotor = 0
+    end 
+    b = b + 1
+  end
+  return decryptedmsg
+end
 
   def decode
 
